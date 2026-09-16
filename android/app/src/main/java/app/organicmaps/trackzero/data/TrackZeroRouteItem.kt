cc062c0666ee81d38dd315b8f427da52d7ba830f
gontary101 <gloyejd477@gmail.com>
2026-09-16 10:02:02 +0200

    feat(ui): add route list card layout and route item data model

---
+++ android/app/src/main/java/app/organicmaps/trackzero/data/TrackZeroRouteItem.kt
@@ -0,0 +1,16 @@
+package app.organicmaps.trackzero.data
+
+import androidx.annotation.DrawableRes
+
+/**
+ * Data model representing a cycling route item displayed in the TrackZero Routes screen.
+ */
+data class TrackZeroRouteItem(
+  val id: String,
+  val title: String,
+  val distanceKm: Double,
+  val elevationGainM: Int,
+  @get:DrawableRes val thumbnailResId: Int? = null,
+  val isFavorite: Boolean = false,
+  val isDownloaded: Boolean = false
+)
